dafc63efc0436e8b6e4e5dd0004d1558942d5934
Poyarkov S <webmaster.cipa@gmail.com>
2018-06-12 23:39:26 +0700

    use one docker-compose file
    Took 1 hour 47 minutes
    
    Took 35 minutes

--- run.sh
+++ run.sh
@@ -16,14 +16,32 @@ set +a
 sudo rm -f ./logs/fpm/*.log ./logs/nginx/*.log ./logs/php/*.log ./logs/postgresql/*.log
 
 # create certificates
-sudo openssl dhparam -out ./conf/certs/dhparam.pem 2048
-sudo openssl req -x509 -nodes -newkey rsa:2048 -days 36500 -keyout ./conf/certs/self-signed.key -out ./conf/certs/self-signed.crt -subj /C=AA/ST=AA/L=Internet/O=MailInABox/CN=APP
+if [[ ! -f "$PWD/conf/certs/dhparam.pem" ]]; then
+    openssl dhparam -out ./conf/certs/dhparam.pem 2048
+fi
+if [[ ! -f "$PWD/conf/certs/self-signed.key" ]] || [[ ! -f "$PWD/conf/certs/self-signed.crt" ]]; then
+    openssl req -x509 -nodes -newkey rsa:2048 -days 36500 -keyout \
+        ./conf/certs/self-signed.key -out \
+        ./conf/certs/self-signed.crt \
+        -subj /C=AA/ST=AA/L=Internet/O=MailInABox/CN=APP
+fi
+
+# define additional variables
+if [[ "$USE_LETSENCRYPT" = "yes" ]]; then
+    export LE_HOST=$SITE_DOMAIN
+fi
+
+if [[ "$APP_MODE" = "prod" ]]; then
+    export DB_NAME=$DB_NAME_PROD
+    export DB_USER=$DB_USER_PROD
+    export DB_PASSWORD=$DB_PASSWORD_PROD
+else
+    export DB_NAME=$DB_NAME_DEV
+    export DB_USER=$DB_USER_DEV
+    export DB_PASSWORD=$DB_PASSWORD_DEV
+fi
 
 # run containers
-if [[ "$USE_LETSENCRYPT" = "yes" ]]; then
-    docker-compose -f docker-compose.le.yml up -d --build
-else
-    docker-compose up -d --build
-fi
+docker-compose -p $COMPOSE_PROJECT_NAME up -d --build
 
 exit 0
